e1f431144d5cead0df235e1cf15d690a83f7f2f3
Matthew Peters <matthew.peters.nyc@gmail.com>
2020-06-26 18:32:40 +0000

    Automatically backed up by Learn

--- lib/count_down.rb
+++ lib/count_down.rb
@@ -1,7 +1,10 @@
 # Write your code here
 count_down = 10
+hny = 0
 
 while count_down > 0 do
+  break if count_down == hny
+  if count_down == hny puts "Happy New Year!"
   puts count_down
   count_down -= 1
 end
